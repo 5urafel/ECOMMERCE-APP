68b5779f74ef758609707dea3b7bc74abf46d7d2
5urafel <surafel5540f@gmail.com>
2025-09-30 20:49:48 +0300

    feat(serverJS): add the backend entry point

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 9000;
+connectDB();
+connectCloudinary();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//API Endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("API Working");
+});
+
+//Listener
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
