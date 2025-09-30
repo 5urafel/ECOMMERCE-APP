a30e1f3901f2427fae53bf0eb6958a95c60162fa
5urafel <surafel5540f@gmail.com>
2025-09-30 20:41:09 +0300

    feat(navbar): add login page link to the profile icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,13 +41,15 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt=""
-          />
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-salt-300 text-gray-500 rounded">
+          <Link to={"/login"}>
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+          </Link>
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 ">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-salt-300 text-gray-500 rounded bg-gray-100">
               <p className="cursor-ponter hover:text-black">My Profile</p>
               <p className="cursor-ponter hover:text-black">Orders</p>
               <p className="cursor-ponter hover:text-black">Logout</p>
