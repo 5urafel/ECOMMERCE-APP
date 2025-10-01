d99ec1c9574bb8d0243d4153cab784129c73389b
5urafel <surafel5540f@gmail.com>
2025-10-01 18:51:30 +0300

    feat: add a product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: Array, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array, required: true },
+  bestseller: { type: Boolean, default: false },
+  date: { type: Date, required: true },
+});
+
+const productModel =
+  mongoose.model.product || mongoose.model("product", productSchema);
+
+export default productModel;
