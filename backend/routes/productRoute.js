00cc64850a241bb3fd2af4e93e4b85a309e7a703
5urafel <surafel5540f@gmail.com>
2025-10-02 21:54:16 +0300

    feat:add product routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,16 +1,31 @@
 import express from "express";
+import multer from "multer";
 import {
   listProducts,
   addProduct,
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
+
 productRouter.get("/list", listProducts);
-productRouter.post("/add", addProduct);
-productRouter.delete("/remove", removeProduct);
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1" },
+    { name: "image2" },
+    { name: "image3" },
+    { name: "image4" },
+  ]),
+  addProduct
+);
+productRouter.delete("/remove", adminAuth, removeProduct);
 productRouter.get("/single", singleProduct);
 
 export default productRouter;
