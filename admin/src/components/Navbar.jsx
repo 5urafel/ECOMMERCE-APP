52fb7ffc1a9005a08658f0d4780ce190895c0b8c
5urafel <surafel5540f@gmail.com>
2025-10-03 21:34:12 +0300

    feat: add logout functionality

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { assets } from "../assets/admin_assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
-    <div className="flex items-center py-2 px-[4%] justify-between">
+    <div
+      onClick={() => setToken("")}
+      className="flex items-center py-2 px-[4%] justify-between border-b-1 border-gray-300"
+    >
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="Logo" />
       <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
         Logout
