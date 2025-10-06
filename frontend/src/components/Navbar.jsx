60792f4ab3df7032d6d5d54a7cac8f1394ff86c6
5urafel <surafel5540f@gmail.com>
2025-10-06 20:32:43 +0300

    feat(navbar): add login , logout and redirect functionalities

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,21 @@ import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const {
+    setShowSearch,
+    getCartCount,
+    token,
+    setToken,
+    navigate,
+    setCartItems,
+  } = useContext(ShopContext);
+
+  const handleLogout = () => {
+    navigate("/login");
+    localStorage.removeItem("token");
+    setToken("");
+    setCartItems({});
+  };
 
   return (
     <div className="flex item-center justify-between py-5 font-medium">
@@ -41,20 +55,36 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <Link to={"/login"}>
-            <img
-              src={assets.profile_icon}
-              className="w-5 cursor-pointer"
-              alt=""
-            />
-          </Link>
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 ">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-salt-300 text-gray-500 rounded bg-gray-100">
-              <p className="cursor-ponter hover:text-black">My Profile</p>
-              <p className="cursor-ponter hover:text-black">Orders</p>
-              <p className="cursor-ponter hover:text-black">Logout</p>
+          <img
+            onClick={() => {
+              if (!token) {
+                navigate("/login");
+              }
+            }}
+            src={assets.profile_icon}
+            className="w-5 cursor-pointer"
+            alt=""
+          />
+
+          {token && (
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 ">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 text-gray-500 rounded bg-slate-100">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p
+                  onClick={() => navigate("/orders")}
+                  className="cursor-pointer hover:text-black"
+                >
+                  Orders
+                </p>
+                <p
+                  onClick={handleLogout}
+                  className="cursor-pointer hover:text-black"
+                >
+                  Logout
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
 
         <Link to="/cart" className="relative">
